33be4937179b28b4ac926e05584d456376da8355
Joe Fryd <joescf@gmail.com>
2016-09-12 16:49:13 -0400

    changed line 5 in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       redirect_to root_path, :notice => 'You have logged in successfully'
     else
       flash.now[:alert] = 'Invalid login/password combination'
